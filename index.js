52259ffd65d17a5d5103c04d257a63ae8fbf7bc5
Ashton Drought <ashtonndrought@yahoo.com>
2023-07-21 21:40:50 -0400

    Adds phase-0-pac-3-arithmetic-lab
    ls

---
+++ index.js
@@ -0,0 +1,58 @@
+function add(){
+
+}
+function subtract(){
+
+} 
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+function add(){
+    return "something";
+}
+function add(a, b) {
+    return "something";
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(){
+    return "something";
+}
+function subract(b, a) {
+    return "something"
+}
+function subtract(b, a) {
+    return b - a;
+}
+function multiply(){
+    return "something"
+}
+function multiply(a, b) {
+    return a * b
+}
+function divide(a, b) {
+    return a / b
+}
+function increment(n) {
+    return "something"
+}
+function increment(n) {
+    return n += 1
+}
+function decrement(n) {
+    return n -= 1
+}
+function makeInt(string) {
+    const result = parseInt(string, 10);
+  return result;
+}
+function preserveDecimal(string){
+    const result = parseFloat(string);
+    return result;
+}
